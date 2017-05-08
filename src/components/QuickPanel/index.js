da052aa5a962fdd26a43ce25fa7af37373bdae2e
DESKTOP-01P0GAI\lotus <lotusms@outlook.com>
2017-05-07 22:32:12 -0400

    no message

--- src/components/QuickPanel/index.js
+++ src/components/QuickPanel/index.js
@@ -3,10 +3,6 @@ import { connect } from 'react-redux';
 import classnames from 'classnames';
 import { Link, hashHistory } from 'react-router';
 import APPCONFIG from 'constants/Config';
-// import {
-//     toggleCollapsedQuickpanel
-// } from '../../actions';
-import QuickPanelContent from './QuickPanelContent';
 
 class QuickPanel extends React.Component {
 
@@ -17,21 +13,8 @@ class QuickPanel extends React.Component {
         });
     }
 
-    // onToggleCollapsedQuickpanel = (e) => {
-    //     const val = !this.props.panelCollapsed;
-    //     const { handleToggleCollapsedPanel } = this.props;
-    //     handleToggleCollapsedPanel(val);
-    // }
-
     render() {
         const { colorOption } = this.props;
-        // let toggleIcon = null;
-        // if (panelCollapsed) {
-        //     toggleIcon = <i className="material-icons">radio_button_unchecked</i>;
-        // } else {
-        //     toggleIcon = <i className="material-icons">radio_button_checked</i>;
-        // }
-
 
         return (
             <div
@@ -40,7 +23,8 @@ class QuickPanel extends React.Component {
                     'bg-color-dark': ['21', '22', '23', '24', '25', '26'].indexOf(colorOption) >= 0
                     })}>
                 <section className="quickpanel-content">
-                    <QuickPanelContent />
+									<div>
+										Quickpanel</div>
                 </section>
             </div>
 
@@ -49,17 +33,9 @@ class QuickPanel extends React.Component {
 }
 
 const mapStateToProps = state => ({
-    // panelCollapsed: state.settings.panelCollapsed,
     colorOption: state.settings.colorOption
-});
-
-const mapDispatchToProps = dispatch => ({
-    // handleToggleCollapsedPanel: (isPanelCollapsed) => {
-    //     dispatch(toggleCollapsedQuickpanel(isPanelCollapsed));
-    // },
 });
 
 module.exports = connect(
     mapStateToProps
-    //mapDispatchToProps
 )(QuickPanel);
